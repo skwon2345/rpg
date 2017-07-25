95edb67348d77832e24c2538cb35a88130e57347
SAMIL CHAI <chai31@chaimac17.local>
2017-07-24 19:33:08 -0700

    Initial Commit

---
+++ rpg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by SAMIL CHAI on 2017. 7. 24..
+//  Copyright © 2017년 SAMIL CHAI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
